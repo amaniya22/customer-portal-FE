5c08c78348b66316e291140bf63f7434f30722e3
amaniya22 <amaniyafaizal@gmail.com>
2025-08-22 10:54:34 +0530

    refresh token bugfix

--- src/routes/protectedRoutes.jsx
+++ src/routes/protectedRoutes.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import PATHS from "./path";
@@ -6,17 +6,20 @@ import { useEffect } from "react";
 import { refreshTokenThunk } from "../redux/slices/userAuthSlice";
 
 const ProtectedRoute = ({ children, requiredRole }) => {
+  const [triedRefresh, setTriedRefresh] = useState(false);
+
   const user = useSelector((state) => state.userAuth?.user);
-  const { status } = useSelector((state) => state.userAuth?.status);
+  const status = useSelector((state) => state.userAuth?.status);
   const role = user?.user_role;
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!user) {
+    if (!user && !triedRefresh) {
       dispatch(refreshTokenThunk());
+      setTriedRefresh(true);
     }
-  }, [user, dispatch]);
+  }, [user, dispatch, triedRefresh]);
 
   if (status === 'loading' || status === 'idle') {
     return <h1>Loading ...</h1>;
